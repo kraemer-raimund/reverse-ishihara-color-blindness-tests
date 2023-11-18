65369f675e1c931d18c59def6f82905b31726e43
Raimund Kraemer <kraemer_raimund@web.de>
2023-11-18 11:21:00 +0100

    Set up canvas for drawing

--- ReverseColorBlindTest/src/main/java/dev/raimundkraemer/colorblind/ColorblindApplication.java
+++ ReverseColorBlindTest/src/main/java/dev/raimundkraemer/colorblind/ColorblindApplication.java
@@ -12,9 +12,10 @@ public class ColorblindApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ColorblindApplication.class.getResource("colorblind-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Reverse Colorblind Test");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- ReverseColorBlindTest/src/main/java/dev/raimundkraemer/colorblind/ColorblindController.java
+++ ReverseColorBlindTest/src/main/java/dev/raimundkraemer/colorblind/ColorblindController.java
@@ -1,5 +1,14 @@
 package dev.raimundkraemer.colorblind;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+
 public class ColorblindController {
 
+    @FXML
+    Canvas canvas;
+
+    public void initialize() {
+        canvas.getGraphicsContext2D().fillOval(0, 0, 100, 100);
+    }
 }
