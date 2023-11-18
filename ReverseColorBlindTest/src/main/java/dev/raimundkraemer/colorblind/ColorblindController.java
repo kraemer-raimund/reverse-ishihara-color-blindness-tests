7248705196dbc44ea703602faf7d95ab8a3b8394
Raimund Kraemer <kraemer_raimund@web.de>
2023-11-18 10:42:50 +0100

    Delete dead code
    It was left over from the generated JavaFX sample created by IntelliJ.

--- ReverseColorBlindTest/src/main/java/dev/raimundkraemer/colorblind/ColorblindController.java
+++ ReverseColorBlindTest/src/main/java/dev/raimundkraemer/colorblind/ColorblindController.java
@@ -1,15 +1,5 @@
 package dev.raimundkraemer.colorblind;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class ColorblindController {
 
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
